459134ff18d4b3568043d2ddae39db9ad4fb6f2e
Prathamesh Jadhav <39017958+prathamesh610@users.noreply.github.com>
2023-01-04 00:32:32 +0530

    initial commit

---
+++ src/main/java/com/prathamesh/kartmalldata/KartmallDataApplication.java
@@ -0,0 +1,13 @@
+package com.prathamesh.kartmalldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartmallDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartmallDataApplication.class, args);
+    }
+
+}
